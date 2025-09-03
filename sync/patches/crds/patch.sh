3061cb11097540d9a0ac03b634055780f269c49c
Simon Weald <glitchcrab-github@simonweald.com>
2025-09-03 10:40:04 +0100

    update CRD subchart version if CRDs change

--- sync/patches/crds/patch.sh
+++ sync/patches/crds/patch.sh
@@ -11,3 +11,23 @@ cd "${repo_dir}"
 
 cp -r "${script_dir}"/manifests/* ./helm/kubeedge-cloudcore/
 cp ./vendor/kubeedge-cloudcore/manifests/charts/cloudcore/crds/* ./helm/kubeedge-cloudcore/charts/kubeedge-cloudcore-crds/templates/
+
+# update the crd Chart.yaml if the CRDs have changed
+CRDS_CHANGED=0
+CRDS_DIR="./helm/kubeedge-cloudcore/charts/kubeedge-cloudcore-crds/templates"
+
+# check for updated CRDs
+if ! git diff --quiet HEAD -- "${CRDS_DIR}"; then
+    CRDS_CHANGED=1
+fi
+
+# check for new CRDs
+if git ls-files --others --exclude-standard -- "${CRDS_DIR}" | grep -q .; then
+    CRDS_CHANGED=1
+fi
+
+# get the current version of the upstream cloudcore chart and update the crd chart version
+if [[ $CRDS_CHANGED -eq 1 ]]; then
+    UPSTREAM_VERSION=$(grep '^version:' vendor/kubeedge-cloudcore/manifests/charts/cloudcore/Chart.yaml | awk '{print $2}')
+    sed -i -E "s/^(version: ).*/\1${UPSTREAM_VERSION}/" "${CRDS_DIR}/Chart.yaml"
+fi
